a0e05052d878fa19957c57769989e33ad4d536f0
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-23 18:44:45 +0200

    font local OK, in resources/fonts (better than in public ? I don't know)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,5 @@
 <p class="font-ibm-plex text-3xl">
     The quick brown fox jumps over the lazy dogs
 </p>
-<p class="font-poppins text-3xl">
-    The quick brown fox jumps over the lazy dogs
-</p>
 
 @endsection
