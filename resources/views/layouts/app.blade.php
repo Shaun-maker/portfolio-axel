e75c6658a4fd3a3c3a0afe1b4910b13202538eb1
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-23 09:47:16 +0200

    first commit, init tallstack

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
+
+    </head>
+    <body class="antialiased">
+        @include('layouts._header')
+
+        <main>
+            @yield('content')
+        </main>
+
+        @include('layouts._footer')
+
+       @livewireScripts
+    </body>
+</html>
