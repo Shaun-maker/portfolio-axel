81b88b47475f4878098f1a06671cb911a30b2ad6
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-23 19:05:35 +0200

    html and basic class for header ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         @livewireStyles
 
     </head>
-    <body class="antialiased">
+    <body class="antialiased text-main">
         @include('partials._header')
 
         <main>
--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,8 +1,8 @@
-<header class="border-solid border-b-2 border-main">
-    <a>
+<header class="border-solid border-b-2 border-main flex items-center">
+    <a href="/" class="text-5xl font-bold text-main border-r-2 border-main basis-1/3 text-center p-10">
         Axel
     </a>
-    <nav>
+    <nav class="basis-2/3 flex justify-end max-w-5xl gap-16">
         <a class="uppercase">
             projets
         </a>
