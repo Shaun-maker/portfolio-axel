94b3cbd46f69e1e177436d970fbc255da87fa4a3
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-26 06:14:44 +0200

    import fontawesome in laravel project

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="bg-main text-white">
     <div class="py-8 relative">
-        <div></div>
+        <div><i class="fa-solid fa-circle"></i><i class="fa-brands fa-github"></i></div>
     </div>
     <p class="">toto</p>
 </footer>
