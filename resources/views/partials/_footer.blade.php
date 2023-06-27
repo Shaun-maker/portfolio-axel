eb54ac3f4628e44a629788208300de92dedc5561
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-27 06:07:24 +0200

    HTML and CSS for footer OK, without responsive and animation

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,9 +1,39 @@
 <footer class="bg-main text-white">
-    <div class="py-8 relative">
+    <div class="py-11 relative border-b-2">
         <div class="text-2xl font-semibold flex items-center">
             <span>Contact</span>
-            <i class="fa-solid fa-circle text-xs"></i>
+            <i class="fa-solid fa-circle text-xs ml-16"></i>
         </div>
     </div>
-    <p class="">toto</p>
+    <section class="flex flex-col items-center gap-16 p-16 border-b-2">
+        <p class="text-2xl">
+            Une idée ? Un projet sur lequel vous avez besoin d'aide ? Une question ?
+        </p>
+        <p class="text-6xl uppercase">
+            contactez-moi
+        </p>
+        <a 
+            href="mailto:contact@axelpaillaud.fr?subject=Contact depuis Portfolio !"
+            class="text-2xl"
+        >
+            contact@axelpaillaud.fr
+        </a>
+        <div class="flex gap-8">
+            <a href="#" class="flex flex-col items-center gap-1">
+                <i class="fa-brands fa-linkedin text-4xl"></i>
+                <span>LinkedIn</span>
+            </a>
+            <a href="#" class="flex flex-col items-center gap-1">
+                <i class="fa-regular fa-file text-4xl"></i>
+                <span>CV</span>
+            </a>
+            <a href="#" class="flex flex-col items-center gap-1">
+                <i class="fa-brands fa-github text-4xl"></i>
+                <span>GitHub</span>
+            </a>
+        </div>
+    </section>
+    <div class="p-8">
+        Made with <i class="fa-solid fa-heart"></i> by Axel
+    </div>
 </footer>
