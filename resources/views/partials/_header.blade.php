0f144c49e0a849d55c36823917d719300979af6a
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-28 06:06:25 +0200

    add animation on hover to link nav menu (for one element)

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -3,7 +3,16 @@
         Axel
     </a>
     <nav class="basis-2/3 flex justify-end max-w-5xl gap-16">
-        <a href="#" class="uppercase">
+        <a 
+            href="#" 
+            class=
+                "relative uppercase 
+                after:absolute after:w-full after:h-px after:bg-main
+                after:left-0 after:translate-y-6 after:opacity-0 
+                after:transition-all after:duration-300
+                hover:after:opacity-100 hover:after:translate-y-5
+            "
+        >
             projets
         </a>
         <a href="#" class="uppercase">
