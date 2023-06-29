7363cf952a347a21af8d168171b3eeffc6643a5b
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-29 05:44:22 +0200

    responsive for header ok

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,8 +1,14 @@
 <header class="border-solid border-b-2 border-main flex items-center">
-    <a href="/" class="text-5xl font-bold text-main border-r-2 border-main basis-1/3 text-center p-10">
+    <a 
+        href="/" 
+        class="sm:text-5xl text-3xl font-bold text-main sm:border-r-2 border-main 
+        basis-1/3 text-center sm:p-10 py-10 px-8"
+    >
         Axel
     </a>
-    <nav class="basis-2/3 flex justify-end max-w-5xl gap-16">
+    <nav 
+        class="basis-2/3 flex justify-end max-w-5xl sm:gap-16 gap-8 sm:mr-8 mr-4"
+    >
         <x-navlink>projets</x-navlink>
         <x-navlink>contact</x-navlink>
     </nav>
