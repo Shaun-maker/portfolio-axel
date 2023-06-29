95ab3c79918eba87d5f89951e1cdc2bbe217e3d2
Axel Paillaud <a.paillaud75@gmail.com>
2023-06-29 10:36:58 +0200

    add test component class has an example

--- app/View/Components/Test.php
+++ app/View/Components/Test.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class TestComponent extends Component
+class Test extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class TestComponent extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.test-component');
+        return view('components.test');
     }
 }
