ef86d16842b335fb66328f9b57bbe92e5a922344
axel-paillaud <contact@axelweb.fr>
2023-06-30 16:56:10 +0200

    add custom tailwind font size for title intro

--- resources/views/partials/_section-intro.blade.php
+++ resources/views/partials/_section-intro.blade.php
@@ -1,5 +1,5 @@
-<section>
-    <h2>Développeur web freelance basé à Orléans</h2>
+<section class="pb-24 pr-24 pl-72 pt-36">
+    <h2 class="text-right text-8.5xl">Développeur web freelance basé à Orléans</h2>
     <div>
         <img src="svg/arrow-down.svg" alt="">
         <p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,9 @@ export default {
         main: '#871414',
         light: '#f3e5e0',
       },
+      fontSize: {
+        '8.5xl': ['5.25rem', '1'],
+      }
     },
     fontFamily: {
       'sans' : ['ibm plex sans', 'sans-serif'],
