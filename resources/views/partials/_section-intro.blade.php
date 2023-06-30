7ddd0ba82306bee3933d3a9d02efaabf91c1cdd0
axel-paillaud <contact@axelweb.fr>
2023-06-30 17:18:36 +0200

    tweak css value for intro section

--- resources/views/partials/_section-intro.blade.php
+++ resources/views/partials/_section-intro.blade.php
@@ -1,8 +1,8 @@
 <section class="pb-24 pr-24 pl-60 pt-36 border-b-2 border-main">
-    <h2 class="text-right text-8.5xl w-4/5 ml-auto mb-24">
+    <h2 class="text-right text-8.5xl w-4/5 ml-auto mb-20">
         Développeur web freelance basé à Orléans
     </h2>
-    <div class="flex gap-32">
+    <div class="flex gap-24">
         <img src="svg/arrow-down.svg" alt="" class="relative top-12">
         <p class="w-1/3">
             Bonjour ! Je suis Axel Paillaud, développeur web passionné.
