d1cc4e3fcb0b363c2a680fd56246e6ee1b30a13a
axel-paillaud <contact@axelweb.fr>
2023-06-30 11:59:56 +0200

    add html for presentation section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,39 @@
 
 @section('content')
 
-<p class="text-3xl h-60 text-green-300">
-    The very quick brown fox jumps over the lazy dogs
-</p>
+{{-- Presentation section --}}
+<section>
+    <div>
+        <p>Axel Paillaud, 28 ans</p>
+        <h3>
+            Fullstack<br>
+            PHP/Laravel<br>
+            Javascript,<br>
+            & more
+        </h3>
+    </div>
+    <div>
+        <picture>
+            <img src="" alt="">
+        </picture>
+        <div>
+            <a href="#" target="_blank">
+                <i class="fa-brands fa-github"></i>
+                <p>GitHub</p>
+            </a>
+            <a href="#" target="_blank">
+                <i class="fa-brands fa-linkedin"></i>
+                <p>LinkedIn</p>
+            </a>
+        </div>
+    </div>
+    <div>
+        <a href="#">Voir mes projets</a>
+        <h3>
+            Développeur web sur Orléans, Loiret. Mobile sur Tours, Blois, Paris.
+            Ouvert au full remote.
+        </h3>
+    </div>
+</section>
 
 @endsection
