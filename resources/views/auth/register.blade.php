02da5129762a271acdc04abb7923e8f1f58cd446
axel-paillaud <contact@axelweb.fr>
2023-07-06 10:24:26 +0200

    implement register form view, plug in to fortify

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,10 +4,17 @@
 <div class="h-[calc(100vh-130px)] flex justify-center items-center">
     <x-auth-form>
         <div class="flex flex-col gap-2">
-            <label for="username">Utilisateur</label>
+            <label for="name">Nom</label>
             <input 
                 class="border border-neutral-300 py-3.5 px-4 focus-visible:outline-main"
-                type="text" required name="username" id="username"
+                type="text" required name="name" id="name"
+            >
+        </div>
+        <div class="flex flex-col gap-2">
+            <label for="email">Email</label>
+            <input 
+                class="border border-neutral-300 py-3.5 px-4 focus-visible:outline-main"
+                type="text" required name="email" id="email"
             >
         </div>
         <div class="flex flex-col gap-2">
@@ -15,6 +22,13 @@
             <input
                 class="py-3.5 px-4 border border-neutral-300 focus-visible:outline-main" 
                 type="password" required name="password" id="password"
+            >
+        </div>
+        <div class="flex flex-col gap-2">
+            <label for="password_confirmation">Confirmation du mot de passe</label>
+            <input
+                class="py-3.5 px-4 border border-neutral-300 focus-visible:outline-main" 
+                type="password" required name="password_confirmation" id="password_confirmation"
             >
         </div>
         <div class="flex justify-center">
---
+++ resources/views/components/auth-form.blade.php
@@ -0,0 +1,7 @@
+<form 
+    method="post"
+    class="flex p-14 bg-light flex flex-col gap-12 rounded-3xl sm:w-[600px] w-[300px]"
+>
+    {{ csrf_field() }}
+    {{ $slot }}
+</form>
