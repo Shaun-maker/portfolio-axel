321e9fdac58a16d555ca14e3ba627b6f666e72b9
axel-paillaud <contact@axelweb.fr>
2023-07-07 11:06:34 +0200

    remove defer parallax attribute

--- resources/js/parallax.js
+++ resources/js/parallax.js
@@ -11,7 +11,6 @@ parallax.target.forEach((parallaxElt) => {
     parallaxElt.ease = parallaxElt.dataset.ease || 0.002;
     parallaxElt.endY = parallaxElt.dataset.endY || 600;
     parallaxElt.direction = parallaxElt.dataset.direction === "up" ? "-" : "";
-    parallaxElt.defer = parallaxElt.dataset.defer || 0;
     parallaxElt.pos = 0;
 
 });
@@ -36,14 +35,14 @@ function updateParallax() {
 
         if (scrollY > parallaxElt.endY) scrollY = parallaxElt.endY;
         
-        parallaxElt.pos += (scrollY - parallaxElt.pos - parallaxElt.defer) * parallaxElt.ease;
+        parallaxElt.pos += (scrollY - parallaxElt.pos) * parallaxElt.ease;
 
         if (Math.abs(scrollY - parallaxElt.pos) < 0.05) {
             parallaxElt.pos = scrollY;
             parallax.parallaxRequest = 0;
         }
 
-        console.log(Math.abs(scrollY - parallaxElt.pos) - 20);
+        console.log(scrollY - parallaxElt.pos)
 
         parallaxElt.style.transform = `translateY(${parallaxElt.direction}${parallaxElt.pos}px)`
 
