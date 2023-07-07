9f722b137ae7fdecd972f8b22502fdb07a432230
axel-paillaud <contact@axelweb.fr>
2023-07-07 14:34:15 +0200

    update 'username' to 'email'

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,10 @@
 <div class="h-[calc(100vh-130px)] flex justify-center items-center">
     <x-auth-form>
         <div class="flex flex-col gap-2">
-            <label for="username">Utilisateur</label>
+            <label for="email">Utilisateur</label>
             <input 
                 class="border border-neutral-300 py-3.5 px-4 focus-visible:outline-main"
-                type="text" required name="username" id="username"
+                type="text" required name="email" id="email"
             >
         </div>
         <div class="flex flex-col gap-2">
