b0c6f37cf632f439872b2441063609610885b581
axel-paillaud <contact@axelweb.fr>
2023-07-07 17:30:23 +0200

    HTML/CSS OK, with hover effect

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,13 +1,17 @@
-<article class="flex max-w-[1400px]">
-    <div class="bg-gray-100 px-24 py-12 basis-5/12 flex justify-center">
+<article class="group flex max-w-[1400px]">
+    <div 
+        class="bg-gray-100 px-24 py-12 basis-5/12 flex justify-center 
+        transition-all duration-300 group-hover:rounded-[32px]"
+    >
         <img 
             src="/images/projects/resize-web-single-image.png" 
             alt="lorem-ipsum-dolor-sit-amet"
-            class="opacity-40 object-cover"
+            class="opacity-40 object-cover transition-all 
+            group-hover:opacity-50 duration-700 group-hover:scale-105"
             width="400" height="400"
         >
     </div>
-    <div class="w-0.5 bg-main mx-10"></div>
+    <div class="w-[2px] bg-main mx-14"></div>
     <div class="basis-7/12 flex flex-col justify-between gap-10">
         <h4 class="text-3xl uppercase text-center">resize web gui</h4>
         <div class="flex flex-col gap-4">
