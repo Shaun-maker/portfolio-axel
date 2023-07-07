8aca047fcf28a28f7089f9f9b26a1dc64bc752d5
axel-paillaud <contact@axelweb.fr>
2023-07-07 16:18:07 +0200

    html for project component ok

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,1 +1,33 @@
-<p>hello, world! </p>
+<article>
+    <div>
+        <img src="" alt="">
+    </div>
+    <div></div>
+    <div>
+        <h4>resize web gui</h4>
+        <div>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+                 sit amet nunc mi. Aliquam a velit eros. Duis volutpat, mi id 
+                 convallis sagittis, erat nibh sollicitudin lorem, sollicitudin 
+                 vestibulum purus tortor nec eros. Curabitur cursus pharetra consequat.
+            </p>
+            <p>
+                Fusce hendrerit rutrum dapibus. Suspendisse et luctus neque. 
+                Aliquam sed eleifend mi, non bibendum nisi. Praesent vel finibus ex. 
+                Quisque vestibulum purus vel diam posuere, ac lacinia justo vulputate. 
+                Sed luctus nisi at dapibus sodales. Aliquam erat volutpat.
+            </p>
+        </div>
+        <div>
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-laravel"></i>
+            <i class="fa-brands fa-php"></i>
+            <i class="fa-brands fa-js"></i>
+        </div>
+        <div>
+            <a>Voir le projet</a>
+            <a>Voir le code source</a>
+        </div>
+    </div>
+</article>
