543613c7d3cf796a8caf78cd2d7311798af4e41a
axel-paillaud <contact@axelweb.fr>
2023-07-07 15:14:30 +0200

    fix responsive for intro section

--- resources/views/partials/_section-intro.blade.php
+++ resources/views/partials/_section-intro.blade.php
@@ -1,13 +1,15 @@
 <section data-parallax data-speed="0.4"
-    class="relative -z-10 pb-24 pr-24 pl-60 pt-72 border-main"
+    class="relative -z-10 lg:pb-24 lg:pr-24 lg:pl-60 lg:pt-72 border-main
+        pb-20 sm:pr-12 sm:pl-32 pt-48 pr-4 pl-4"
 >
     <h2 id="js-split-text" 
-        class="text-8.5xl w-4/5 ml-auto mb-20 flex flex-end gap-5 flex-wrap invisible">
+        class="text-5xl sm:text-8.5xl lg:w-4/5 ml-auto mb-20 flex justify-end gap-5 flex-wrap
+         invisible">
         Développeur web basé à Orléans.
     </h2>
     <div class="flex gap-24">
         <img src="svg/arrow-down.svg" alt="" class="relative top-12">
-        <p class="w-1/3 animate-fadeIn opacity-0" 
+        <p class="sm:w-1/3 animate-fadeIn opacity-0" 
             style="animation-delay: 1s; animation-fill-mode: forwards;">
             Bonjour ! Je suis Axel Paillaud, développeur web passionné.
             Explorez mon portfolio pour en savoir plus sur mon travail !
