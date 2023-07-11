e2c711b227b4eac2a7ecf22c747a00e24d616cd0
axel-paillaud <contact@axelweb.fr>
2023-07-11 10:00:26 +0200

    add clockworks packages

--- resources/views/partials/_section-projects.blade.php
+++ resources/views/partials/_section-projects.blade.php
@@ -3,7 +3,7 @@
     <x-marquee-project />
 
     {{-- Filter here, TODO --}}
-    <div class="h-72"></div>
+    <div class="h-40"></div>
 
     <div class="sm:px-12 gap-24 px-6 my-20 flex flex-col justify-center items-center">
         @foreach($projects as $project)
