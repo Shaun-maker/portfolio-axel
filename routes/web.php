da5482fbca03ae6c8d0ed7a32c0b669a4cd8bdae
axel-paillaud <contact@axelweb.fr>
2023-07-11 08:41:30 +0200

    update HomeController : create invoke() function to load home view

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-/* Route::get('/assets/fonts/IBMPlexSans-Regular.ttf', function () {
-    $path = public_path('assets/fonts/' . "IBMPlexSans-Regular.ttf");
-
-    if (file_exists($path)) {
-        return response()->file($path);
-    }
-
-    abort(404);
-}); */
-
-Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('/home', function () {
-    return view('home');
-});
+Route::get('/', HomeController::class);
