f5f2093fcd757f8ee40bbdbbe584a1d1eb9281a5
axel-paillaud <contact@axelweb.fr>
2023-07-11 13:35:18 +0200

    overflowhidden wip

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,6 +1,5 @@
 let filterClip = document.getElementById('js-filter-clip');
 let filterBtn = document.getElementById('js-filter-btn');
-let parentClip = document.getElementById('js-parent-clip');
 
 filterBtn.addEventListener('click', (e) => {
     filterClip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)"
--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -4,25 +4,27 @@
     border-main';
 @endphp
 
-<div id="js-parent-clip" class="relative px-12">
-    <button id="js-filter-btn"
-        {{ $attributes->merge(['class' => $classes]) }}
-    >
-        <i 
-            class="fa-solid fa-circle absolute text-[0] group-hover/link:text-xs 
-            transition-all duration-700 ease-out left-7">
-        </i>
-        <span 
-            class="group-hover/link:translate-x-3 transition-transform duration-700
-            ease-out whitespace-nowrap"
+<div class="relative px-12">
+    <div class="overflow-hidden">
+        <button id="js-filter-btn"
+            {{ $attributes->merge(['class' => $classes]) }}
         >
-            {{ $slot }}
-        </span>
-    </button>
-    <div
-        id="js-filter-clip"
-        class="bg-main absolute inset-0 transition-all duration-500"
-        style="clip-path: polygon(0 0, 19% 100%, 0 100%, 0 0);"
-    >
+            <i 
+                class="fa-solid fa-circle absolute text-[0] group-hover/link:text-xs 
+                transition-all duration-700 ease-out left-7">
+            </i>
+            <span 
+                class="group-hover/link:translate-x-3 transition-transform duration-700
+                ease-out whitespace-nowrap"
+            >
+                {{ $slot }}
+            </span>
+        </button>
+        <div
+            id="js-filter-clip"
+            class="bg-main absolute inset-0 transition-all duration-500"
+            style="clip-path: polygon(0 0, 19% 100%, 0 100%, 0 0);"
+        >
+        </div>
     </div>
 </div>
