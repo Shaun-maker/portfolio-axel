3358b1425991ca126d33f8db1d7e9244376f732e
axel-paillaud <contact@axelweb.fr>
2023-07-11 08:47:04 +0200

    load projects fakedata to the home view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class HomeController extends Controller
 {
     public function __invoke() 
     {
-        return ('home');
+        return view('home', [
+            'projects' => Project::latest()->get(),
+        ]);
     }
 }
