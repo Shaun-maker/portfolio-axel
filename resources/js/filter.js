494a75f610e0a519dca930e92a3b8625375a27cf
axel-paillaud <contact@axelweb.fr>
2023-07-11 14:30:07 +0200

    fill effect ok, circle appear wip

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,6 +1,32 @@
 let filterClip = document.getElementById('js-filter-clip');
 let filterBtn = document.getElementById('js-filter-btn');
+let circle = document.getElementById('js-circle');
+
+let testBtn = document.getElementById('js-test-btn');
+
+let fill = false;
+
+testBtn.addEventListener('click', (e) => {
+    if (!fill) {
+        filterClip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
+        setTimeout(() => {
+            filterBtn.style.color = "white";
+            circle.style.top = "-30px";
+        }, 200);
+        fill = true;
+    }
+    else {
+        filterClip.style.clipPath = "polygon(0 0, 19% 100%, 0 100%, 0 0)";
+        setTimeout(() => {
+            filterBtn.style.color = "#871414";
+        }, 200);
+        circle.style.top = "50%";
+
+        fill = false;
+    }
+});
 
 filterBtn.addEventListener('click', (e) => {
-    filterClip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)"
+    filterClip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
+
 });
--- resources/views/components/filter.blade.php
+++ resources/views/components/filter.blade.php
@@ -4,27 +4,40 @@
     border-main';
 @endphp
 
-<div class="relative px-12">
-    <div class="overflow-hidden">
+{{-- This main div is to hide circle when selected --}}
+<div class="relative">
+
+    {{-- Hide outside clip-path --}}
+    <div class="overflow-hidden relative rounded-full z-20">
         <button id="js-filter-btn"
             {{ $attributes->merge(['class' => $classes]) }}
         >
             <i 
                 class="fa-solid fa-circle absolute text-[0] group-hover/link:text-xs 
-                transition-all duration-700 ease-out left-7">
+                transition-all duration-700 ease-out left-7 z-30">
             </i>
             <span 
-                class="group-hover/link:translate-x-3 transition-transform duration-700
-                ease-out whitespace-nowrap"
+                class="filter-transition group-hover/link:translate-x-3
+                 ease-out whitespace-nowrap z-30"
             >
                 {{ $slot }}
             </span>
         </button>
         <div
             id="js-filter-clip"
-            class="bg-main absolute inset-0 transition-all duration-500"
+            class=" z-20 bg-main absolute top-0 right-0 bottom-0 -left-6 transition-all duration-500"
             style="clip-path: polygon(0 0, 19% 100%, 0 100%, 0 0);"
         >
         </div>
     </div>
+
+    {{-- circle animation when filter is selected --}}
+    <i 
+        id="js-circle"
+        class="fa-solid fa-circle absolute center-absolute z-10 transition-all
+        duration-500"
+    >
+    </i>
 </div>
+
+<button id='js-test-btn'>Test</button>
