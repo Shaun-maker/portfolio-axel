2c2b947270080ef012fc20400862027c2d23d0ef
axel-paillaud <contact@axelweb.fr>
2023-07-11 17:47:44 +0200

    refacto filter.js code, fill by default first element of categoryFilter

--- resources/js/filter.js
+++ resources/js/filter.js
@@ -1,72 +1,51 @@
-/* let filterClip = document.getElementById('js-filter-clip');
-let filterBtn = document.getElementById('js-filter-btn');
-let circle = document.getElementById('js-circle'); */
-
 let filterButtons = document.querySelectorAll('[data-filter-button]');
 
-
 filterButtons.forEach((filterButton) => {
+
+    if(filterButton.dataset.fill === 'true') {
+        enableBtnFilter(filterButton);
+    }
 
     filterButton.addEventListener('click', (e) => {
         filterButtons.forEach((filterButton) => {
             if (filterButton.dataset.fill === "true") {
-                let clip = filterButton.closest('[data-filter]').querySelector('[data-clip');
-                let circle = filterButton.closest('[data-filter]').querySelector('[data-circle]');
-
-                clip.style.clipPath = "polygon(0 0, 19% 100%, 0 100%, 0 0)";
-                setTimeout(() => {
-                    filterButton.style.color = "#871414";
-                }, 200);
-                circle.style.top = "8px";
-        
-                filterButton.dataset.fill = "false";
+                disableBtnFilter(filterButton);
             }
         })
     })
 
     filterButton.addEventListener('click', () => {
 
-        let clip = filterButton.closest('[data-filter]').querySelector('[data-clip');
-        let circle = filterButton.closest('[data-filter]').querySelector('[data-circle]');
-
         if (filterButton.dataset.fill === "false") {
-            clip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
-            setTimeout(() => {
-                filterButton.style.color = "white";
-                circle.style.top = "-30px";
-            }, 200);
-            filterButton.dataset.fill = "true";
+            enableBtnFilter(filterButton);
         }
 
     })
 
 })
 
-/* let testBtn = document.getElementById('js-test-btn'); */
+function disableBtnFilter(filterButton)
+{
+    let clip = filterButton.closest('[data-filter]').querySelector('[data-clip');
+    let circle = filterButton.closest('[data-filter]').querySelector('[data-circle]');
 
-/* let fill = false;
+    clip.style.clipPath = "polygon(0 0, 19% 100%, 0 100%, 0 0)";
+    setTimeout(() => {
+        filterButton.style.color = "#871414";
+    }, 200);
+    circle.style.top = "8px";
 
-testBtn.addEventListener('click', (e) => {
-    if (!fill) {
-        filterClip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
-        setTimeout(() => {
-            filterBtn.style.color = "white";
-            circle.style.top = "-30px";
-        }, 200);
-        fill = true;
-    }
-    else {
-        filterClip.style.clipPath = "polygon(0 0, 19% 100%, 0 100%, 0 0)";
-        setTimeout(() => {
-            filterBtn.style.color = "#871414";
-        }, 200);
-        circle.style.top = "8px";
+    filterButton.dataset.fill = "false";
+}
 
-        fill = false;
-    }
-});
+function enableBtnFilter(filterButton) {
+    let clip = filterButton.closest('[data-filter]').querySelector('[data-clip');
+    let circle = filterButton.closest('[data-filter]').querySelector('[data-circle]');
 
-filterBtn.addEventListener('click', (e) => {
-    filterClip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
-
-}); */
+    clip.style.clipPath = "polygon(100% 0, 100% 100%, 0 100%, 0 0)";
+    setTimeout(() => {
+        filterButton.style.color = "white";
+        circle.style.top = "-30px";
+    }, 200);
+    filterButton.dataset.fill = "true";
+}
