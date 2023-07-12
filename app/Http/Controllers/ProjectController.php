88bbcd9e750ca82c7ec6d1a4a461afb057b6fc90
axel-paillaud <contact@axelweb.fr>
2023-07-12 09:49:59 +0200

    ProjectResource, ProjectController 'show()' function OK, API routes doesn't work for the moment

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Resources\ProjectResource;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function show($id)
+    {
+        return new ProjectResource(Project::findOrFail($id));
+    }
 }
---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'category_id' => $this->category_id,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'title' => $this->title,
+            'url_image' => $this->url_image,
+            'description' => $this->description,
+            'project_link' => $this->project_link,
+            'source_link' => $this->source_link,
+        ];
+    }
+}
