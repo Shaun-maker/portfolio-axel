d0984afc727fc5c17e8abc3684c9d53f50e2f74f
axel-paillaud <contact@axelweb.fr>
2023-07-13 15:57:39 +0200

    edit button now open the good modal

--- resources/js/modal.js
+++ resources/js/modal.js
@@ -1,8 +1,20 @@
 let editButtons = document.querySelectorAll('[data-edit-button');
+let introEditModal = document.getElementById('js-intro-modal');
+let presentationEditModal = document.getElementById('js-presentation-modal');
+let projectEditModal = document.getElementById('js-project-modal');
 
 const openModal = function(event) {
     let editButton = event.target.dataset.editButton;
-    console.log(editButton);
+    
+    if(editButton === "intro") {
+        introEditModal.showModal();
+    }
+    else if (editButton === "presentation") {
+        presentationEditModal.showModal();
+    }
+    else if (editButton === "project") {
+        projectEditModal.showModal();
+    }
 }
 
 editButtons.forEach(button => {
