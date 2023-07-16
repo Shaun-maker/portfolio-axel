d11d3d9a0b833df7cbb1b5d19b20fa023fccf278
axel-paillaud <contact@axelweb.fr>
2023-07-16 17:47:24 +0200

    refacto

--- resources/views/components/tool-item.blade.php
+++ resources/views/components/tool-item.blade.php
@@ -1,5 +1,9 @@
 <li class=" hover:bg-light whitespace-nowrap w-full py-2 px-4 transition-colors">
-    <button data-tools-id="{{ $tool->id }}" type="button">
+    <button 
+        x-on:click="open = ! open"
+        data-tools-id="{{ $tool->id }}" 
+        type="button"
+        >
         <i class="{{ $tool->icon }} text-xl mr-1.5"></i>
         {{ $tool->name }}
     </button>
