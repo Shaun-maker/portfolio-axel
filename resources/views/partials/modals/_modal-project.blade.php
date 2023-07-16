23be36913647fa45e9598be2ed21201f20e2cb57
axel-paillaud <contact@axelweb.fr>
2023-07-16 11:49:57 +0200

    hide other modal, set project modal to 'open'

--- resources/views/partials/modals/_modal-project.blade.php
+++ resources/views/partials/modals/_modal-project.blade.php
@@ -1,1 +1,23 @@
-<p>Quentin</p>
+<div class="text-main">
+    <h5 class="uppercase text-center">projets</h5>
+    {{-- Grid container --}}
+    <div>
+        {{-- Grid child --}}
+        <div>
+            <img
+                class="relative" 
+                src="" 
+                alt=""
+            >
+            <button class="absolute">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+            <button>
+                <i class="fa-solid fa-pen-to-square"></i>
+                éditer
+            </button>
+        </div>
+    </div>
+    <hr>
+    <x-cta-button>ajouter un projet</x-cta-button>
+</div>
