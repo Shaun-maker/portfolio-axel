c584de49b155bc0042dd7673e6e548edd367c7cc
axel-paillaud <contact@axelweb.fr>
2024-07-01 16:51:27 +0200

    refacto smooth-scroll javascript code

--- resources/js/home/smooth-scroll.js
+++ resources/js/home/smooth-scroll.js
@@ -1,15 +1,13 @@
 import Lenis from 'lenis';
 
-const lenis = new Lenis();
-
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+const lenis = new Lenis({
+    // duration: 2,
+});
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+    lenis.raf(time);
+    requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
+requestAnimationFrame(raf);
+
