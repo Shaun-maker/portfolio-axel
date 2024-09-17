34d29c46a4b07852aeae8f671ba85248bdc989c2
axel-paillaud <contact@axelweb.fr>
2024-09-17 16:12:41 +0200

    refacto presentation: HTML/CSS OK, responsive ok

--- resources/views/partials/_section-presentation.blade.php
+++ resources/views/partials/_section-presentation.blade.php
@@ -9,8 +9,8 @@
 {{-- top-[calc(100vh-650px)] --}}
 <section
     id="js-presentation"
-    class="flex flex-col xl:flex-row px-6 pb-16 bg-light gap-8 relative
-     transition-all duration-[2000ms] ease-out top-[calc(100vh-650px)] justify-center"
+    class="px-6 pb-2 bg-light relative transition-all duration-[2000ms]
+    ease-out top-[calc(100vh-650px)]"
 >
 
     @auth
@@ -22,10 +22,10 @@
     @endauth
 
     {{-- Description and picture --}}
-    <div class="flex flex-col-reverse gap-16 sm:gap-24 xl:flex-row xl:gap-52">
+    <div class="flex flex-col-reverse gap-16 sm:gap-24 xl:flex-row xl:gap-52 justify-center">
 
         {{-- Description --}}
-        <div class="xl:pt-16 mx-auto sm:w-[512px] xl:w-auto w-72 max-w-[512px]">
+        <div class="xl:pt-16 mx-auto xl:mx-0 sm:w-[512px] xl:w-auto w-72 max-w-[512px]">
             <p><b>Axel Paillaud</b>, 30 ans.</p>
             <p>
                 Développeur web indépendant sur Orléans,<br>
@@ -44,7 +44,7 @@
         {{-- Picture --}}
         <div
             class="border-x-2 border-b-2 border-main overflow-hidden
-            sm:w-[512px] sm:h-[512px] sm:max-w-fit max-w-72 w-full h-72 mx-auto"
+            sm:w-[512px] sm:h-[512px] sm:max-w-fit max-w-72 w-full h-72 mx-auto xl:mx-0"
         >
             <picture class="block">
                 <source srcset="{{ $profile->url_image_webp }}" type="image/webp">
@@ -60,23 +60,25 @@
     </div>
 
     {{-- CTA and social media --}}
-<!--     <div class="flex w-full justify-center gap-16 mt-4">
-        <x-social-media
-            class="fa-brands fa-github"
-            href="https://github.com/axel-paillaud"
-        >
-            GitHub
-        </x-social-media>
-        <x-social-media
-            class="fa-brands fa-linkedin"
-            href="https://www.linkedin.com/in/axel-paillaud/"
-        >
-            LinkedIn
-        </x-social-media>
-        <x-cta-link class="self-start" href="/assets/cv/AxelPaillaud.pdf">
+    <div class="flex w-full justify-center flex-col sm:flex-row gap-16 mt-32 mb-40 items-center">
+        <div class="flex gap-16">
+            <x-social-media
+                class="fa-brands fa-github group-hover:scale-110 transition"
+                href="https://github.com/axel-paillaud"
+            >
+                GitHub
+            </x-social-media>
+            <x-social-media
+                class="fa-brands fa-linkedin group-hover:scale-110 transition"
+                href="https://www.linkedin.com/in/axel-paillaud/"
+            >
+                LinkedIn
+            </x-social-media>
+        </div>
+        <x-cta-link href="/assets/cv/AxelPaillaud.pdf">
             télécharger mon cv
         </x-cta-link>
-    </div> -->
+    </div>
 
     <!--     {{-- Bloc 1 : Name and stack --}}
 
